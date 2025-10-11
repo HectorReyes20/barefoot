22092ccd997951adaaae07562149e48cb99defc5
Josthyn <josthyndiaz45@gmail.com>
2025-10-11 16:52:36 -0500

    subiendo mis cambios desde IntelliJxd

--- src/main/java/com/barefoot/model/Producto.java
+++ src/main/java/com/barefoot/model/Producto.java
@@ -84,4 +84,124 @@ public class Producto {
         }
         return 0;
     }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public Double getPrecioDescuento() {
+        return precioDescuento;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public Boolean getDestacado() {
+        return destacado;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public LocalDateTime getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public void setPrecioDescuento(Double precioDescuento) {
+        this.precioDescuento = precioDescuento;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public void setDestacado(Boolean destacado) {
+        this.destacado = destacado;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public void setFechaActualizacion(LocalDateTime fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
 }
