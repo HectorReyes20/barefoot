154cf4e2a4c2623ba93d3c1b4a54906351ef510f
Hector Reyes <mpea.reyeshector@gmail.com>
2025-11-19 17:16:13 -0500

    Merge con los cambios de joao

---
+++ src/main/java/com/barefoot/controller/PersonalizarController.java
@@ -0,0 +1,17 @@
+package com.barefoot.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PersonalizarController {
+
+    @GetMapping("/personalizar")
+    public String mostrarPersonalizar(HttpSession session, Model model) {
+        model.addAttribute("nombreUsuario", session.getAttribute("usuarioNombre"));
+        return "personalizar";
+    }
+
+}
